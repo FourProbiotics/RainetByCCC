9948a1bb6b05252080ddcce6f013416bae2be782
FourProbiotics <476965207@qq.com>
2016-06-30 16:17:15 +0800

    Revert "update"
    This reverts commit e6b96c324f9d10be0f9cb2890e87ec4b73708b1c.

--- assets/Script/massAction.js
+++ assets/Script/massAction.js
@@ -1,4 +1,3 @@
-<<<<<<< 4e2296c30ed5f09098bb0092dcaa77b43ee7abf4
 cc.Class({
     extends: cc.Component,
 
@@ -26,32 +25,3 @@ cc.Class({
 
     // },
 });
-=======
-cc.Class({
-    extends: cc.Component,
-
-    properties: {
-    },
-
-    // use this for initialization
-    onLoad: function () {
-        var self = this;
-        var particle = self.getComponent(cc.ParticleSystem);
-        particle.startColor = cc.Color(255,0,0);
-        
-        this.node.opacity = 0;
-        var func = new cc.CallFunc(
-            function(){
-                particle.stopSystem();
-            }
-        ,this);
-        var seq = cc.sequence(cc.delayTime(3), func);
-        this.node.runAction(seq);
-    },
-
-    // called every frame, uncomment this function to activate update callback
-    // update: function (dt) {
-
-    // },
-});
->>>>>>> origin
--- assets/Script/scaleBarListener.js
+++ assets/Script/scaleBarListener.js
@@ -1,4 +1,3 @@
-<<<<<<< 4e2296c30ed5f09098bb0092dcaa77b43ee7abf4
 cc.Class({
     extends: cc.Component,
 
@@ -40,46 +39,3 @@ cc.Class({
             content.y = (targetH-747)/2;
     },
 });
-=======
-cc.Class({
-    extends: cc.Component,
-
-    properties: {
-        board: cc.Node,
-        scrollContent: cc.Node
-    },
-
-    // use this for initialization
-    onLoad: function () {
-
-    },
-
-
-    scrolled: function(){
-        var x = this.node.parent.x;
-        this.scaleMap((855+x)/570);
-    },
-
-    // 对棋盘进行缩放
-    // @scale: 缩放参数
-    scaleMap: function(scale){
-
-        scale = scale < 1 ? 1 : (scale > 2 ? 2 : scale);
-        
-        var content = this.scrollContent;
-        var board = this.board;
-
-        board.scale = scale;
-        let targetW = scale*615, targetH = scale*747;
-        content.setContentSize(targetW, targetH);
-        if(content.x < (615-targetW)/2)
-            content.x = (615-targetW)/2;
-        if(content.x > (targetW-615)/2)
-            content.x = (targetW-615)/2;
-        if(content.y < (747-targetH)/2)
-            content.y = (747-targetH)/2;
-        if(content.y > (targetH-747)/2)
-            content.y = (targetH-747)/2;
-    },
-});
->>>>>>> origin
